9502112195daa95127fc29fdd59a589aef44a019
NickZhirnoff <nickzhirnoff@gmail.com>
2025-07-01 20:49:34 +0300

    Removed the mock catalog screen

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -4,12 +4,6 @@ final class TabBarController: UITabBarController {
 
     var servicesAssembly: ServicesAssembly!
 
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(systemName: "square.stack.3d.up.fill"),
-        tag: 0
-    )
-    
     private let statisticsTabBarItem = UITabBarItem(
         title: "Статистика",
         image: UIImage(systemName: "flag.2.crossed.fill"),
@@ -18,21 +12,15 @@ final class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let catalogController = TestCatalogViewController(
-            servicesAssembly: servicesAssembly
-        )
         
         let statisticsPresenter = StatisticsPresenter(service: servicesAssembly)
         let statisticsController = StatisticsViewController(presenter: statisticsPresenter)
         statisticsPresenter.delegate = statisticsController
         
         let nc = UINavigationController(rootViewController: statisticsController)
-        
-        catalogController.tabBarItem = catalogTabBarItem
         nc.tabBarItem = statisticsTabBarItem
 
-        viewControllers = [catalogController, nc]
+        viewControllers = [nc]
 
         view.backgroundColor = .systemBackground
     }
